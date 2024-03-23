99567705b201b246877550d4b3ea23c1a88a2094
abhishekkumarraj966 <abhishekkumarraj62073@gmail.com>
2024-03-23 13:11:19 +0530

    sdfgh

--- src/Component/Blogs.jsx
+++ src/Component/Blogs.jsx
@@ -7,7 +7,7 @@ const Blogs = () => {
          <p className='text-center font-bold text-[15px] text-white lg:text-[32px] '>Blogs</p>
          <div className='flex justify-between mt-[25px] lg:mt-[50px] '>
             <div>
-                <img src='/assets/unsplash_eg726v9y3iA.svg' className='w-[111px] h-[117px] rounded-[20px] lg:w-[325px] lg:h-[342px]'/>
+                <img src='https://s3-alpha-sig.figma.com/img/69a4/a49f/c0bb45a0fed98270ec95e4c6df14aea4?Expires=1711929600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Xc0HGnhD9K5tyfac0MINgXbAA09wSQ9tNjAPF2MKttB7hOWSwdZ5TYY2FjNrlUliBP9-0TCBrw9rAM3nV3uAVhQ9HCtuLObiapjdIf81hAk0w7siRZDK9YpYvYcRQl4ytpaJc7xHa3SxRMHXFqKNHzK2X6qsbNXHUTRsZXpTsJosoM8TIGR065iOAvZYnUiFTTpDSW5GPx2Pb9Ec1-fs7X0aixOohuQJ25~68NEtKb-jDYsOPInI7ygBFWsTyt4o9ySWnfhrqPvjw3bHPa-oY0ulSV2MHRczZUoTz5OIuyzEjjUb0b5viz-NjdDW7~xjsJdoaglQ-jtdIeJAHVHLKw__' className='w-[111px] h-[117px] rounded-[20px] lg:w-[325px] lg:h-[342px]'/>
                 <p className='font-bold text-center text-[7px] lg:text-[20px] text-white mt-[10px] lg:mt-[20px]'>HandMade Decor</p>
             </div>
             <div>
