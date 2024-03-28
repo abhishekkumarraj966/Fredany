6ed7403461a20d4242dc2c638c0bfd5a08c1594e
abhishekkumarraj966 <abhishekkumarraj62073@gmail.com>
2024-03-28 21:04:12 +0530

    fghj

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,34 +1,86 @@
-import React from 'react'
-
+import React from "react";
+import QR from "./assites/QR.jpeg";
 const Footer = () => {
-    return (
-        <>
-        
-        <div className='w-full h-[180px]  sm:h-[250px] lg:h-[330px] bg-[#D4B294] flex flex-col items-center justify-center'>
-            <div className='w-[90%] lg:w-[60%] mx-auto flex justify-between text-[#7D3C13]'>
-                <a href='' className='font-normal text-[12px] sm:text-[18px] lg:text-[24px] list-none'>About us</a>
-                <a href='' className='font-normal text-[12px] sm:text-[18px] lg:text-[24px]' >Contact us</a>
-                <a href='' className='font-normal text-[12px] sm:text-[18px] lg:text-[24px] '>privacy policy</a>
-                <a href='' className='font-normal text-[12px] sm:text-[18px] lg:text-[24px] '>Terms & conditions</a>
-            </div>
-            <div className='w-full lg:w-[75%] flex justify-around ml-auto mt-[20px] sm:mt-[28px] lg:mt-[45px]'>
-                <div className='flex items-center gap-2 lg:gap-6'>
-                    <a href='' className='text-[#7D3C13] font-bold text-[16px] sm:text-[18px] lg:text-[24px]'>Connect with us on :-</a>
-                    <a href=''><img src='/assets/WhatsApp (2).png' className='w-[26px] lg:w-[56px] sm:h-[35px]  sm:w-[35px]h-[26px] lg:h-[55px]' /></a>
-                    <a href=''><img src='/assets/Instagram (2).png' className='w-[26px] lg:w-[56px] sm:h-[35px]  sm:w-[35px]h-[26px] lg:h-[55px]' /></a>
-                    <a href=''><img src='/assets/Facebook (2).png' className='w-[26px] lg:w-[56px] sm:h-[35px]  sm:w-[35px]h-[26px] lg:h-[55px]' /></a>
-                </div>
-                <div>
+  return (
+    
+      <div className="w-full h-[880px]  sm:h-[350px] lg:h-[410px] bg-[#D4B294] flex flex-col ">
+        <div class="grid grid-cols-1 sm:grid-cols-3  gap-4">
+          <div className=" lg:mt-24 sm:mt-24 text-center mt-4">
+            <h1 className="font-bold text-6xl sm:text-4xl lg:text-6xl pt-5 md:pt-0 cursor-pointer">
+              <span className="text-8xl sm:text-6xl font-medium">F</span>
+              <span className="text-[#012429]">R</span>
+              <span className="text-[#19272b]">E</span>
 
-                    <img src='/assets/Group 32968.png' className='w-[40px] lg:w-[107px] sm:w-[70px] sm:h-[71px] h-[40px] lg:h-[107px]' />
+              <span className="text-[#43131D]">D</span>
+            </h1>
+          </div>
+          <div className="lg:mb-0 mt-10 lg:mt-10 mx-5 sm:mx-0">
+            <p className="mb-6 text-xl font-bold ">Contaact Details :-</p>
+            <ul className="flex flex-col space-y-2   ">
+              <h1 className=" font-bold text-xl">Address</h1>
+              <li className=" hover:text-black text-lg cursor-pointer">
+                shop 19, fred decor,Ahmedabad
+              </li>
+              <h1 className=" font-bold text-xl">phone</h1>
+              <li className="hover:text-black cursor-pointer text-lg">+91 915 793 2201</li>
+              <h1 className=" font-bold text-xl">Email.</h1>
+              <li className=" hover:text-black text-lg cursor-pointer">
+                fashionista@gmail.com
+              </li>
+            </ul>
+          </div>
 
-                </div>
-            </div>
+          <div className=" mb-2 sm:mb-8 lg:mb-0  lg:mt-10 mx-5 sm:mx-0 ">
+            <p className="mb-4 text-lg font-semibold  pt-12 ">Like & Follow</p>
+            <ul className="flex flex-col space-y-4 text-[14px] font-medium ">
+              <span className="flex flex-row gap-3 sm:gap-3 md:gap-3 lg:gap-3 xl:gap-3 mt-2">
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/9d16/2623/8c5a9a99136881eb51a187c52d3e4ed4?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hy95RoIl1DYKlKliaPskkH7KK7SqvSmaqGqte6GTGqLuGXnrllYY5U-La2wjJIwLLDqw-xKrMYXW~bho2cZkQiIyOZYvXXPMEKM~6xe-58IxAOJZRh5UO0SyonUFgaTbjCa4B7k34-ZmO9RUjeEPJY65a84BdT3mEbrXx~6sK2h6X9njQshHEtt4qF~40741P-K7hSoNgGVe96ZHjFrYmVAOWdmzSj~1RC0f~njYgAljvBDoTMK3Ojh71FTiX-MBdlgkou6Yjwx91OtfQGjNTsreVKMH54OA1cKMyV7c80XYVk~JEqwlYY94AZn-Sumv6lLivMTE3vRhJsKiUFi4KA__"
+                  alt="facebook logo"
+                  className="w-[25px] lg:w-[32px] sm:w-[25px] sm:h-[25px] h-[25px] lg:h-[32px] cursor-pointer"
+                />
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/bcee/4e49/91e8a49f05f4ad5c94cbcc56c08ad6da?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=MtXoo82SdWdm55NucZ2qpRWopHA~nLkQlYUhtBoAH~Kz~LRGUgRBD21SDFliTeyR8TWyFrZm0ddGrCIpb-1kP8gpSYhvEOdjeTA0SSroW5mnrfpr-cS-q~INoELAhS0AMPAOLIeA5yOf4hb4yS8SfMvyT1mamjMykrkQlKa0bEc2InTv7kXbP~TZ5NeJyv0s2GS~vFko9UPvHYV5tMvagqEfr-CuaOJd4oicrq5BeRWebVEfQ7tqGY5LpBY1sYyJx8~Hvj07kKW3MR5Te1l6EucrT6DVuST6h8BHvhWPpUv5XGLLATXeOSarD57ZcX3rGEkHHst~dtvJHpZzVMYfyw__"
+                  alt="facebook logo"
+                  className="w-[25px] lg:w-[32px] sm:w-[25px] sm:h-[25px] h-[25px] lg:h-[32px] cursor-pointer"
+                />
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/4aec/2ff6/e4e1fb762e4e278383bfe9245eee4fbf?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p5qVNtUxNgQuUFyDMMalUb0FQz9ehVt8NPN-BaVBDqhHu5PMg4GI1CqJ6ZXH9E3B5aG~vSz1iYWCfeb~sU10zzR4xPIXXUtFxiUBwsLtCEZLpF0a7ZnooJ8RXWVlWLwXQfQWYrt60d7fgDlaYkSdsyJF62pGSE-9nx8rF2nSCgh7yTotjfXwjMBlAzxwnc1R2SgTDKxtfTStM6yM72UAXRLUcGVDMnuUuEchq1A~Na6vPWAoW5ItXC9CRtQuqUBhth7BI313wm9VmvX-JSRdqXB03Ys1udCkfZdlqUP2YGHYhsRwtoyGSd5rANu30bYZmFZdO-ncwR87iiM34T6Tpg__"
+                  alt="insta"
+                  className="w-[25px] lg:w-[32px] sm:w-[25px] sm:h-[25px] h-[25px] lg:h-[32px] cursor-pointer"
+                />
+                <img
+                  src="https://s3-alpha-sig.figma.com/img/0e6d/a98f/298b85a49c48d2e0a595c0f7dd2b12c2?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=giWBo0ZACkMWn2oEsZFwqjjZ74ncuhoPfaktn4wqZ-XH7GWY-SIi8vS22T4t4WM41l-PoCFSMg2-ZN3Y7QFx7r7zQXvPYGiZXmegQhTsMtep7HANpyqKGal3epuOmMniht-cYl0ygej624AC2Xu9~0mf-0JjLoSxVNkyiV7strBnpVEePtYwn39m~FhRUpoMtimKntnhzM1ILUbpPNXZ8CFmiuP3-zAOhjUfIR5uclynqfH3IpzimXXojn-rMVr3yIZWGWxdDtg4l~pCHZCPmgJDVzmeFt75PZYoR4hdSL4H8NLe9lXEJaIje3Vz09bwI3lW7XyzeEeSGHzxUovXBA__"
+                  alt="youtube"
+                  className="w-[25px] lg:w-[32px] sm:w-[25px] sm:h-[25px] h-[25px] lg:h-[32px] cursor-pointer"
+                />
+              </span>
+              <li className="text-2xl font-bold">PAY NOW</li>
+              <li>
+                <a href="https://www.figma.com/file/1qPHan6lbYD57Cz9KuYQJ9/image/5a7d0214bdd3d9de2cb78b0a3e001fed5bec74ab">
+                  <img
+                    src={QR}
+                    alt="QR"
+                    className="m-2 h-[120px] w-[#130px] pb-5 cursor-pointer"
+                  />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
+        <hr className="my-1 bg-black text-black" />
+      <div className="mx-auto max-w-6xl items-center justify-between px-4 md:flex lg:px-0 mb-1">
+        <div className="mt-4 md:mt-0">
+          <p className="text-sm font-medium ">
+            © 2023 DevUI. All rights anyintech.
+          </p>
+        </div>
+      </div>
+      </div>
 
-            <p className='text-center pb-4 bg-[#D4B294] text-[#7D3C13] font-light text-[12px] sm:text-[16px] lg:text-[20px] '>© 2023 All Right Reserved. Developed By FredDecor</p>
-        </>
-    )
-}
+      
+  
+  );
+};
 
-export default Footer
+export default Footer;
